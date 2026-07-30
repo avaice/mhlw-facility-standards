90b0d8552da7b542ec52dcfc604572721a1c6151
Tomoaki Hirabayashi <tomoaki.hirabayashi@medley.jp>
2026-07-30 21:54:41 +0900

    feat: init base

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
 export function App() {
-  return <p className="p-4 text-lg font-bold text-sky-700">hello, world</p>;
+  return (
+    <div className="space-y-6">
+      <header>
+        <h1 className="text-3xl font-bold">医療機関 施設基準検索</h1>
+      </header>
+      <main>
+        <div className="flex gap-2">
+          <input
+            className="w-full p-3 border border-gray-300 rounded-md text-lg"
+            type="text"
+            placeholder="医療機関名または10桁の医療機関コードを入力"
+          />
+          <button className="p-3 bg-blue-500 text-white rounded-md hover:bg-blue-600 shrink-0 w-16">
+            検索
+          </button>
+        </div>
+      </main>
+    </div>
+  );
 }
